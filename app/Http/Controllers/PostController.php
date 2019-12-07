8196956fab996b00e4726f17f29aed9f47c80bc6
fadjriaf <fadjrialfalah@gmail.com>
2019-12-07 15:14:24 +0700

    update posts
    adding auth and slug in post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = Post::all();
+
+        return view('index')->withPosts($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request, [
+            'title'         => 'required|min:3|max:255',
+            'slug' => 'required|min:3|max:255|unique:posts',
+            'description'   => 'required|min:3'
+        ]);
+
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->slug, '-');
+        $post->description = $request->description;
+
+        $post->save();
+
+        return redirect()->route('index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        //
+        $post = Post::where('slug', $slug)->first();
+
+        return view('post.show')->withPost($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($slug)
+    {
+        //
+        $post = Post::where('slug', $slug)->first();
+
+        return view('post.edit')->withPost($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $slug)
+    {
+        //
+        $this->validate($request, [
+            'title'         => 'required|min:3|max:255',
+            'slug' => 'required|min:3|max:255|unique:posts,id,' . $slug,
+            'description'   => 'required|min:3'
+        ]);
+
+        $post = Post::where('slug', $slug)->first();
+
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->slug, '-');
+        $post->description = $request->description;
+
+        $post->save();
+
+        return redirect()->route('post.show', $post->slug);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $post = Post::find($id);
+
+        $post->delete();
+
+        return redirect()->route('index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PostController@index')->name('index');
+
+// Route Post
+Route::get('/post/create', 'PostController@create')->name('post.create');
+Route::post('/post/create', 'PostController@store')->name('post.store');
+Route::get('/post/{id}/edit', 'PostController@edit')->name('post.edit');
+Route::post('/post/{id}/edit', 'PostController@update')->name('post.update');
+Route::post('/post/{id}/destroy', 'PostController@destroy')->name('post.destroy');
+
+Route::get('/post/{slug}', 'PostController@show')->name('post.show');
+Route::get('/post/{slug}/edit', 'PostController@edit')->name('post.edit');
+Route::post('/post/{slug}/edit', 'PostController@update')->name('post.update');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
